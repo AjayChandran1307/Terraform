a785836ecdae04d09bda17fec849e0737a31734d
AjayChandran1307 <115857973+AjayChandran1307@users.noreply.github.com>
2025-04-27 13:33:41 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -82,13 +82,7 @@ resource "azurerm_virtual_machine" "VM" {
   resource_group_name   = azurerm_resource_group.TerRG.name
   network_interface_ids = [azurerm_network_interface.NIC.id]
   vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
+  
   storage_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
